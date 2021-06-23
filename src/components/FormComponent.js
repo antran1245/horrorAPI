b11d80bf13044bcb33f8630fc60084db554adc15
antran1245 <antran1245@gmail.com>
2021-06-22 17:17:49 -0700

    basic display of result

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Container, Row, Col, Card, Form, Button } from 'react-bootstrap';
+import ResultDisplay from './ResultComponent';
 
 function ButtonTypes(props) {
     return(
@@ -18,19 +19,22 @@ class HorrorForm extends Component {
             Comedy: false,
             Thriller: false,
             Drama: false,
-            Mystery: false,
+            Crime: false,
             from: 0,
             to: 0,
             exact: 0,
             info: "",
-            database: []
+            database: [],
+            showModal: false,
+            passInfo: [],
+            genres: []
         };
-        
+        this.submitButton = this.submitButton.bind(this);
         this.buttonTypes = this.buttonTypes.bind(this);
     }
 
     componentDidMount() {
-        fetch("https://antran1245.github.io/data/movies.json")
+        fetch("http://my-json-server.typicode.com/antran1245/horrorAPI/movies")
         .then(res => res.json())
         .then(data => {
             this.setState({
@@ -48,7 +52,38 @@ class HorrorForm extends Component {
         this.setState({
             [event.target.name]: (this.state[event.target.name] === false ? true : false)
         });
-        console.log(this.state.database[0]);
+        if(this.state[event.target.name] === false) {
+            this.setState({
+                genres: this.state.genres.concat(event.target.name)
+            });
+        } else if(this.state[event.target.name] === true) {
+            this.setState({
+                genres: this.state.genres.filter(item => item !== event.target.name)
+            });
+        }
+    }
+
+    submitButton() {
+        var result = []
+        if(this.state.Comedy === true) {
+            result = this.state.database.filter(item => item['Genres'].includes('Comedy'));
+        } else {
+            result = result.concat(this.state.database);
+        }
+        if(this.state.Thriller === true) {
+            result = result.filter(item => item['Genres'].includes('Thriller'));
+        }
+        if(this.state.Drama === true) {
+            result = result.filter(item => item['Genres'].includes('Drama'));
+        }
+        if(this.state.Crime === true) {
+            result = result.filter(item => item['Genres'].includes('Crime'));
+        }
+        this.setState({
+            showModal: true,
+            passInfo: result
+        });
+        console.log(result);
     }
 
     render() {
@@ -64,7 +99,7 @@ class HorrorForm extends Component {
                                 <ButtonTypes onClick={this.buttonTypes} state={this.state.Comedy} name={"Comedy"}/>
                                 <ButtonTypes onClick={this.buttonTypes} state={this.state.Thriller} name={"Thriller"}/>
                                 <ButtonTypes onClick={this.buttonTypes} state={this.state.Drama} name={"Drama"}/>
-                                <ButtonTypes onClick={this.buttonTypes} state={this.state.Mystery} name={"Mystery"}/>
+                                <ButtonTypes onClick={this.buttonTypes} state={this.state.Crime} name={"Crime"}/>
                             </Row>
                         </Form.Group>
                         <Form.Group>
@@ -90,11 +125,12 @@ class HorrorForm extends Component {
                         </Form.Group>
                         <Form.Row className="justify-content-center">
                             <Col xs={12} sm={{span:6}}>
-                                <Button block>Submit</Button>
+                                <Button block onClick={this.submitButton}>Submit</Button>
                             </Col>
                         </Form.Row>
                         </Form>
                     </Card>
+                    <ResultDisplay show={this.state.showModal} info={this.state.passInfo} hide={()=>this.setState({showModal: false})}/>
                 </Col>
                 </Row>
             </Container>
--- src/components/ResultComponent.js
+++ src/components/ResultComponent.js
@@ -1,14 +1,27 @@
-import React, {Component} from 'react';
-import {Modal, Container, Row, Col, Button} from 'react-bootstrap';
+import React from 'react';
+import {Modal, Container} from 'react-bootstrap';
 
-class ResultDisplay extends Component {
-    render() {
+function ResultDisplay(props){
         return(
-            <Modal>
+            <Modal show={props.show} onHide={props.hide} style={{maxHeight: "100%"}}>
                 <Container fluid>
-
+                    <h1 style={{textAlign:"center"}}>Movies</h1>
+                    <Modal.Body>
+                        {props.info.map(element =>{
+                            return <React.Fragment key={element['Title']}>
+                                <h3>{element['Title']}</h3>
+                                <h5>{element['Genres'].replace('Horror|','').replaceAll('|', ',')}</h5>
+                                <h5>{element['Release Date']} , {element['Release Country']}</h5>
+                                <h5>{element['Movie Rating']}</h5>
+                                <h5>{element['Movie Run Time']}</h5>
+                                <h5>{element['Plot']}</h5>
+                                <h5>{element['Cast'].replaceAll('|', ', ')}</h5>
+                            </React.Fragment>
+                        })}
+                    </Modal.Body>
                 </Container>
             </Modal>
         );
     }
-}
+
+export default ResultDisplay;
